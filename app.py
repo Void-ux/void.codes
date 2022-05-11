e9a6fe4fe55a963ccab935b0bfbf60977a79b809
Void-ux <daniel.grant2017@gmail.com>
2022-05-11 20:24:49 +0800

    Removed LFG webhooks and whatnot

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, config, redirect, request, render_template, jsonify
-from discord import SyncWebhook, File
 
 app = Flask(__name__)
-VOTE_WEBHOOK = SyncWebhook.from_url(vote_webhook_url)
-PATREON_WEBHOOK = SyncWebhook.from_url(patreon_webhook_url)
 
 
 @app.route("/")
