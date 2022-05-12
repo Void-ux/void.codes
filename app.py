6ec8fc9230cc2475f0ad7348991bf4fc766c57dc
Void-ux <daniel.grant2017@gmail.com>
2022-05-12 19:12:21 +0800

    Wrong attribute

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        print(request.json)
+        print(request.data)
 
 
 @app.route('/chiefron')
